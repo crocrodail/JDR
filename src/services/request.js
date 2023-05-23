606cbafcccbe662030cb98f0c8ab8de581cc0a45
crocrodail <31371620+crocrodail@users.noreply.github.com>
2023-05-23 19:43:32 +0200

    Update request.js

--- src/services/request.js
+++ src/services/request.js
@@ -1,10 +1,18 @@
+import Recoil from 'recoil';
+import { userState } from './../store/userStore';
+
 export default function Request(url, body) {
+    const [, setUser] = Recoil.useRecoilState(userState);
     return new Promise((resolve) => {
         fetch(url, body)
             .then((response) => {
                 if (response.ok) {
                     return response.json();
                 } else {
+                    if (response.status === 401) {
+                        localStorage.removeItem('user');
+                        setUser(null);
+                    }
                     throw new Error('Something went wrong');
                 }
             })
